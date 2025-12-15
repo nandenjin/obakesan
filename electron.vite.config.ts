12696469cdbd28e83a6104f8c7d3b2686d34732b
Kazumi Inada <hello@nandenjin.com>
2025-12-16 02:29:27 +0900

    fix: Mark `consola` to be bundled at electron-vite config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,13 +1,14 @@
 import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "electron-vite";
 
-export default {
+export default defineConfig({
   main: {
     build: {
       lib: {
         entry: "./src/main/main.ts",
       },
-      rollupOptions: {
-        external: ["ftdi-d2xx"],
+      externalizeDeps: {
+        include: ["ftdi-d2xx"],
       },
     },
   },
@@ -16,9 +17,12 @@ export default {
       lib: {
         entry: "./src/preload/preload.ts",
       },
+      externalizeDeps: {
+        exclude: ["consola"],
+      },
     },
   },
   renderer: {
     plugins: [vue()],
   },
-};
+});
