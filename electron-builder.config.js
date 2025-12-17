3deeda301240014205915ec371c3481ddea71fca
Kazumi Inada <hello@nandenjin.com>
2025-12-17 10:15:18 +0900

    chore: Add suffix to build artifact

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -16,24 +16,25 @@ module.exports = {
   // afterSign: 'build/notarize.js',
   win: {
     target: ["zip"],
+    artifactName: "obakesan-${version}-win.${ext}",
   },
   mac: {
     notarize: !!process.env.ENABLE_APPLE_NOTARIZATION,
     icon: "build/icon_mac.png",
-    artifactName: "obakesan-${version}.${ext}",
+    artifactName: "obakesan-${version}-mac.${ext}",
   },
   dmg: {
-    artifactName: "obakesan-${version}.${ext}",
+    artifactName: "obakesan-${version}-mac.${ext}",
   },
   linux: {
     executableName: "obakesan",
-    artifactName: "obakesan-${version}.${ext}",
+    artifactName: "obakesan-${version}-linux.${ext}",
     target: ["AppImage", "snap", "deb"],
     maintainer: "Kazumi Inada <hello@nandenjin.com>",
     category: "Utility",
   },
   appImage: {
-    artifactName: "obakesan-${version}.${ext}",
+    artifactName: "obakesan-${version}-linux.${ext}",
   },
   npmRebuild: false,
 };
