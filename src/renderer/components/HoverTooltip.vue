1be2676a52e29eae79dfafc72f83a0745f24f970
Kazumi Inada <hello@nandenjin.com>
2025-12-17 13:29:28 +0900

    fix: Avoid overflow of tooltip

--- src/renderer/components/HoverTooltip.vue
+++ src/renderer/components/HoverTooltip.vue
@@ -9,11 +9,14 @@
   </span>
   <Teleport to="body">
     <div
-      v-if="hover && !disabled && content"
+      ref="tooltip"
       class="tooltip"
+      :class="{
+        'is-active': hover && !disabled && content,
+      }"
       :style="{
-        left: target?.getBoundingClientRect().left + 'px',
-        top: target?.getBoundingClientRect().bottom + 'px',
+        left: `${left}px`,
+        top: `${top}px`,
       }"
     >
       {{ content }}
@@ -22,15 +25,43 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, useTemplateRef } from "vue";
+import { ref, useTemplateRef, watch } from "vue";
 
 defineProps<{
   disabled?: boolean;
   content?: string | null;
 }>();
 
-const target = useTemplateRef("target");
+const target = useTemplateRef<HTMLElement>("target");
+const tooltip = useTemplateRef<HTMLElement>("tooltip");
 const hover = ref(false);
+
+const left = ref(0);
+const top = ref(0);
+
+const updatePosition = () => {
+  if (!target.value || !tooltip.value) return;
+
+  const targetRect = target.value.getBoundingClientRect();
+  const tooltipRect = tooltip.value.getBoundingClientRect();
+
+  const leftMax = window.innerWidth - tooltipRect.width - 5;
+  const leftMin = 5;
+
+  left.value = Math.min(
+    Math.max(targetRect.left - tooltipRect.width / 2, leftMin),
+    leftMax
+  );
+
+  top.value = targetRect.bottom;
+};
+
+watch(hover, async (newVal) => {
+  if (newVal) {
+    // Determine position immediately
+    updatePosition();
+  }
+});
 </script>
 
 <style scoped>
@@ -42,5 +73,14 @@ const hover = ref(false);
   border: 1px solid #888;
   transform: translate(0, 5px);
   z-index: 999;
+  width: max-content;
+  max-width: 300px;
+  pointer-events: none;
+  transition: opacity 0.1s ease;
+
+  &:not(.is-active) {
+    opacity: 0;
+    visibility: hidden;
+  }
 }
 </style>
